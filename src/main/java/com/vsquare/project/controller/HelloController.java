32a733e3926a5cb50b05d80d04f412fd87479825
Vj <vijeyyuvaraj@gmail.com>
2024-06-07 22:49:16 +0530

    Initial commit

---
+++ src/main/java/com/vsquare/project/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.vsquare.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String welcome(){
+        return "Backend API running!";
+    }
+}
